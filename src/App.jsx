9b611c5db65f08684fff9045dd075389ff8589f8
Denchik <buddy.den@mail.ru>
2025-07-24 16:35:23 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './velik.css'
+
+function App() {
+    return (
+        <>
+            <div className="container">
+                <p className="content">ПРИВОЗИ ВЕЛИК</p>
+                <hr className="horizon"/>
+            </div>
+        </>
+    );
+}
+
+export default App;
